9e2a1d844b1ffb8d28bc04d538152a2883d6cf5d
ganeshphatangare-rw <79680237+ganeshphatangare-rw@users.noreply.github.com>
2021-08-16 18:10:13 +0530

    Update COS bucket url
    Updated the COS bucket URL with latest code fixes done for routing issues in Ubuntu 18 migration.

--- rmm-template.tf
+++ rmm-template.tf
@@ -4,7 +4,7 @@ variable "TF_VERSION" {
 }
 
 variable "image_url" {
-  default = "cos://us-east/rackware-rmm-bucket/RackwareP2PBYOLTemplate.qcow2"
+  default = "cos://us-east/rackware-rmm-bucket/RackWareP2PBYOL.qcow2"
 }
 
 /**
